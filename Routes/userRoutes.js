4422119362140031a7de2ec8cd2c93f6b7097de5
Инсаф Садиков <sadikovinsaf99@gmail.com>
2024-01-26 15:44:17 +0300

    wd

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,10 +6,22 @@ const userAuth=require("../Middleware/userAuth")
 const router=new Router()
 const {signup, login}=userController
 
+const test=(req, res)=>{ 
+     try{ 
+        res.status(201).json(" The test went through")
+     }catch(e){
+        res.status(501).json("An error occurred")
+     }
+}
+
 router.post("/signup",userAuth.saveUser, signup)
 router.post("/login", login)
-router.get("/checkauth", userController.checkAuth)
 router.post("/logout",userController.logout)
+
+router.get("/checkauth", userController.checkAuth)
+router.get("/test", test)
+
+
 
 module.exports=router
 
