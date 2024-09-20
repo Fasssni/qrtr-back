c70446264d0950b640aaf37f2908f94b3c7fe252
Fasssni <sadikovinsaf99@gmail.com>
2024-09-20 20:29:33 +0300

    chore: adjust

--- Routes/botRoutes.js
+++ Routes/botRoutes.js
@@ -19,13 +19,14 @@ const {
   getUserTemplates,
   addTemplate,
 } = require("../Controllers/botControllers");
+const { isAuth } = require("../Middleware/userAuth");
 
-router.post("/sendmessage", sendMessage);
+router.post("/sendmessage", isAuth, sendMessage);
 router.post("/addtemplate", addTemplate);
 
 router.get("/getmgs", getMessages);
 router.get("/conversations", getConversations);
-router.get("/getchat/:id", getUserChat);
+router.get("/getchat/:id", isAuth, getUserChat);
 router.get("/gettemplates/:bot_id", getUserTemplates);
 router.post(`/createbot`, createBotInstance);
 router.delete("/clearchat", clearChat);
